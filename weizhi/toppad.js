3b3fd65d3d8de83567764eaa304a5a6e514cd41e
fuyanbing <1204689638@qq.com>
2016-11-10 16:07:24 +0800

    添加位置

---
+++ weizhi/toppad.js
@@ -0,0 +1,28 @@
+if ((navigator.userAgent.match(/(iPhone|iPod|Android|ios|iOS|iPad|Backerry|WebOS|Symbian|Windows Phone|Phone)/i))) { 
+
+}else{
+/*Helloweba-ÑÝÊ¾Ò³-640*60*/
+//document.write('<script type="text/javascript">var cpro_id = "u2431048";</script>');
+//document.write('<script src="http://cpro.baidustatic.com/cpro/ui/c.js" type="text/javascript"></script>');
+
+
+document.write("<script type=\"text/javascript\">google_ad_client = \"ca-pub-7515443544894528\";google_ad_slot = \"3076591508\";google_ad_width = 468;google_ad_height = 60;</script>");
+document.write("<script type=\"text/javascript\" src=\"//pagead2.googlesyndication.com/pagead/show_ads.js\"></script>");
+}
+
+if ((navigator.userAgent.match(/(iPhone|iPod|Android|ios|iOS|iPad|Backerry|WebOS|Symbian|Windows Phone|Phone)/i))) { 
+    document.write("<script type=\"text/javascript\">google_ad_client = \"ca-pub-7515443544894528\";google_ad_slot = \"8321126704\";google_ad_width = 300;google_ad_height = 250;</script>");
+	document.write("<script type=\"text/javascript\" src=\"//pagead2.googlesyndication.com/pagead/show_ads.js\"></script>");
+}else{ 
+    document.write("<script type=\"text/javascript\">google_ad_client = \"ca-pub-7515443544894528\";google_ad_slot = \"3438097725\";google_ad_width = 728;google_ad_height = 90;</script>");
+	document.write("<script type=\"text/javascript\" src=\"//pagead2.googlesyndication.com/pagead/show_ads.js\"></script>");
+} 
+
+
+
+
+
+//document.write("<script type=\"text/javascript\" src=\"http://js.tongji.linezing.com/1870888/tongji.js\"></script>");
+document.write("<script src=\"http://s13.cnzz.com/stat.php?id=4524941&amp;web_id=4524941\" language=\"javaScript\"></script>");
+
+document.write('<script>var _hmt = _hmt || [];(function() {var hm = document.createElement("script");hm.src = "//hm.baidu.com/hm.js?33de7cee069f421e70f7986cb117eece";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(hm, s);})();</script>');
